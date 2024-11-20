a076fbfa4b89780197585d11e3b29a974c6dda86
Aarthi-web-dev <161298934+Aarthi-web-dev@users.noreply.github.com>
2024-11-20 11:01:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskManager from './TaskManager.js'
+
+function App() {
+  return (
+    <>
+  <TaskManager/>
+    </>
+   );
+}
+
+export default App;
---
+++ src/TaskManager.js
@@ -0,0 +1,281 @@
+import React, { useState } from "react";
+
+const TaskManager = () => {
+  const [activeButton, setActiveButton] = useState("all");
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(true); // Manage theme state
+
+  const completedTasksCount = tasks.filter((task) => task.selected).length;
+
+  const handleButtonClick = (button) => {
+    setActiveButton(button);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTask = () => {
+    if (inputValue.trim()) {
+      setTasks([...tasks, { text: inputValue, selected: false }]);
+      setInputValue(""); // Clear input after adding
+    }
+  };
+
+  const toggleTaskSelection = (index) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, selected: !task.selected } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    if (activeButton === "all") return true;
+    if (activeButton === "completed") return task.selected;
+    if (activeButton === "active") return !task.selected;
+    return true;
+  });
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const backgroundImage = isDarkMode
+    ? 'url("https://todolist333.vercel.app/img/day1.jpeg")'
+    : 'url("https://todolist333.vercel.app/img/night1.jpeg")';
+  const overlayColor = isDarkMode ? "rgba(139,92,246,0.5)" : "rgba(139,92,246,0.5)";
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        background: `
+          linear-gradient(to bottom,
+          ${overlayColor} 42%,
+          ${isDarkMode ? "white" : "black"} 40%),
+          ${backgroundImage}`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top",
+        color: isDarkMode ? "black" : "white" ,
+        padding: "20px",
+      }}
+    >
+      {/* Total Task Counter and Theme Toggle */}
+      <div style={{ marginBottom: "20px", fontWeight: "bold", fontSize: "18px" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span
+            style={{
+              color: "white",
+              fontSize: "40px",
+              padding: "20px",
+              marginLeft: "360px",
+              textAlign:"center"
+            }}
+          >
+            TODAY  TASKS...
+          </span>
+          <span
+            onClick={toggleTheme}
+            style={{
+              marginLeft: "180px",
+              width: "20px",
+              height: "20px",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+              color: isDarkMode ? "white" : "white",
+              fontSize: "26px",
+            }}
+          >
+            {isDarkMode ? "☾" : "☼"}
+          </span>
+        </div>
+      </div>
+
+      {/* First box  */}
+      <div style={{ position: "relative", marginBottom: "10px", marginLeft:"380px", }}>
+        <input
+          type="text"
+          placeholder="Create a new task"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={(e) => e.key === "Enter" && addTask()} // Add task on Enter key
+          style={{
+            width: "50%",
+            height:"35px",
+            padding: "8px",
+            paddingRight: "30px",
+            paddingLeft: "30px",
+            borderRadius:"5px",
+            fontSize: "16px",
+            backgroundColor: isDarkMode ? "#F1F1EC" : "black",
+            color: isDarkMode ? "black" : "white",
+            border: `1px solid ${isDarkMode ? "white" : "black"}`,
+          }}
+        />
+        {inputValue && (
+          <span
+            onClick={() => setInputValue("")} // Clear input
+            style={{
+              position: "absolute",
+              left: "10px",
+              top: "50%",
+              transform: "translateY(-50%)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: inputValue ? "20px" : "10px",
+              height: inputValue ? "20px" : "10px",
+              borderRadius: "50%",
+              backgroundColor: "red",
+              cursor: "pointer",
+              fontSize: inputValue ? "14px" : "10px",
+              color: "#fff",
+            }}
+          >
+            &#10005;
+          </span>
+        )}
+      </div>
+
+      {/* Second box  */}
+      <div
+        style={{
+          backgroundColor: isDarkMode ? "white" : "black",
+          marginTop: "30px",
+          padding: "20px",
+          marginLeft:"380px",
+          width:"37%",
+          boxShadow:'5px 5px 10px #555 ',
+        //   height:"100px",
+
+          
+        }}
+      >
+        {tasks.length > 0 && (
+          <div
+            style={{
+              marginBottom: "10px",
+              fontWeight: "bold",
+              color: isDarkMode ? "black" : "white",
+            }}
+          >
+            Total Tasks: {tasks.length}, Completed: {completedTasksCount}
+          </div>
+        )}
+        <div style={{ display: "flex", marginBottom: "10px" }}>
+          <button
+            onClick={() => handleButtonClick("all")}
+            style={{
+              backgroundColor: activeButton === "all" ? "rgb(139,92,246)" : isDarkMode ? "white" : "white",
+              color: activeButton === "all" ? "white" : "rgb(139,92,246)",
+              padding: "8px 16px",
+              border: "2px solid rgb(139,92,246)",
+              fontWeight:'bold',
+              marginRight: "25px",
+              cursor: "pointer",
+              borderRadius:"4px",
+            }}
+          >
+            All
+          </button>
+          <button
+            onClick={() => handleButtonClick("active")}
+            style={{
+              backgroundColor: activeButton === "active" ? "rgb(139,92,246)" : isDarkMode ? "white" : "white",
+              color: activeButton === "active" ? "white" : "rgb(139,92,246)",
+              padding: "8px 16px",
+              border: "2px solid rgb(139,92,246)",
+              fontWeight:'bold',
+              marginRight: "25px",
+              cursor: "pointer",
+              borderRadius:"4px",
+
+            }}
+          >
+            Active
+          </button>
+          <button
+            onClick={() => handleButtonClick("completed")}
+            style={{
+              backgroundColor: activeButton === "completed" ? "rgb(139,92,246)" : isDarkMode ? "white" : "white",
+              color: activeButton === "completed" ? "white" : "rgb(139,92,246)",
+              padding: "8px 16px",
+              border: "2px solid rgb(139,92,246)",
+              fontWeight:'bold',
+              cursor: "pointer",
+              borderRadius:"4px",
+
+            }}
+          >
+            Completed
+          </button>
+        </div>
+
+        {/* Task List */}
+        <div>
+          {filteredTasks.map((task, index) => (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "8px",
+                marginBottom: "5px",
+                borderBottom: `1px solid ${isDarkMode ? "black" : "white"}`,
+                borderRadius: "4px",
+              }}
+            >
+              <span
+                onClick={() => toggleTaskSelection(index)}
+                style={{
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  border: `1px solid ${isDarkMode ? "black" : "black"}`,
+                  marginRight: "10px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  backgroundColor: task.selected
+                    ? "purple"
+                    : isDarkMode
+                    ? "white"
+                    : "white",
+                }}
+              >
+                {task.selected ? "✓" : ""}
+              </span>
+              <span style={{ flex: 1 }}>{task.text}</span>
+              <button
+                onClick={() => deleteTask(index)}
+                style={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                  color: "red",
+                  cursor: "pointer",
+                }}
+              >
+                &#10005;
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskManager;
